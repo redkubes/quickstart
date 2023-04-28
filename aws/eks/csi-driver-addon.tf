24b16cd0b8656a1800aedf7d1dac59e99da6b9b9
Leia Renee <leia.renee@redkubes.com>
2023-04-28 14:29:53 +0200

    fix: csi cluster name and ignore values.yaml

--- aws/eks/csi-driver-addon.tf
+++ aws/eks/csi-driver-addon.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_addon" "csi_driver" {
-  cluster_name             = "otomi-eks-quickstart"
+  cluster_name             = var.cluster_name
   addon_name               = "aws-ebs-csi-driver"
   addon_version            = "v1.11.4-eksbuild.1"
   service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
